395e4b0f367800bfa4f6ad4d365c7d9ce4d12935
Nguyen Hoang Nam <nguyenhoangnam.dev@gmail.com>
2021-10-10 21:52:54 +0700

    🎉 Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Mini-File-Icons
+
+A subset of [File-Icons-VSCode](https://github.com/file-icons/vscode). Like [File-Icons-VSCode](https://github.com/file-icons/vscode), this font uses several icons from [Devicons](https://github.com/vorillaz/devicons), [Mfixx](https://github.com/fizzed/font-mfizz), [Octicons](https://github.com/primer/octicons), [Nonicons](https://github.com/yamatsum/nonicons) and of cource [File-Icons](https://github.com/file-icons/icons).
+
+This font is extremely personal choice, so if you want more or less then fork it and change.
+
+![Screenshot](https://raw.githubusercontent.com/Nguyen-Hoang-Nam/readme-image/main/mini-file-icons/mini-file-icon.png)
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+You can find license of icons in icons directory.
---
+++ main.js
@@ -0,0 +1,71 @@
+import SVGIcons2SVGFontStream from "svgicons2svgfont";
+import svg2ttf from "svg2ttf";
+
+import { readdir, stat, writeFile } from "fs/promises";
+import { createReadStream } from "fs";
+
+const getAllIcons = async (path, icons = [[], []]) => {
+    const files = await readdir(path);
+    for (const file of files) {
+        const stats = await stat(path + "/" + file);
+
+        if (stats.isDirectory()) {
+            icons = await getAllIcons(path + "/" + file, icons);
+        } else {
+            const filetype = file.split(".")[1];
+            if (filetype === "svg") {
+                icons[1].push(file);
+                icons[0].push(path + "/" + file);
+            }
+        }
+    }
+
+    return icons;
+};
+
+const firstUTF = "e900";
+let data = "";
+
+(async () => {
+    const fontStream = new SVGIcons2SVGFontStream({
+        fontName: "mini-file-icons",
+        normalize: true,
+        fontHeight: 1000,
+    });
+
+    const icons = await getAllIcons("./icons/");
+    const [iconPaths, iconNames] = icons;
+
+    const iconsLength = iconPaths.length;
+
+    // Credit https://stackoverflow.com/a/63886620/16065010
+    const indices = Array.from(iconNames.keys());
+    indices.sort((a, b) => iconNames[a].localeCompare(iconNames[b]));
+
+    let utf = firstUTF;
+
+    for (let i = 0; i < iconsLength; i++) {
+        const iconIndex = indices[i];
+        const glyph = createReadStream(iconPaths[iconIndex]);
+        glyph.metadata = {
+            unicode: [utf],
+            name: iconNames[iconIndex],
+        };
+
+        fontStream.write(glyph);
+
+        // Credit https://stackoverflow.com/a/31439284/16065010
+        utf = (parseInt(utf, 16) + 1).toString(16);
+    }
+
+    fontStream.end();
+
+    fontStream.on("data", (chunk) => {
+        data += chunk;
+    });
+
+    fontStream.on("end", async () => {
+        const ttf = svg2ttf(data, {});
+        await writeFile("./fonts/mini-file-icons.ttf", Buffer.from(ttf.buffer));
+    });
+})();
